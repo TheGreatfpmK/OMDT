719b71f0443922081564dc07869c86124048c5a4
Filip Macak <filip.macak1999@gmail.com>
2024-10-03 23:54:40 +0200

    dtcontrol parser init script (update it when adding models with trees)

---
+++ run-dtcontrol-parser.sh
@@ -0,0 +1,13 @@
+# python run-qcomp.py dtcontrol-parser consensus-2-8 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+# python run-qcomp.py dtcontrol-parser consensus-3-32 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+python run-qcomp.py dtcontrol-parser csma-2-4 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+python run-qcomp.py dtcontrol-parser firewire-3 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+python run-qcomp.py dtcontrol-parser ij-10 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+# python run-qcomp.py dtcontrol-parser ij-12 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+# python run-qcomp.py dtcontrol-parser pacman --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+# python run-qcomp.py dtcontrol-parser philosophers-3 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+python run-qcomp.py dtcontrol-parser pnueli-zuck-3 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+# python run-qcomp.py dtcontrol-parser rabin-3 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+python run-qcomp.py dtcontrol-parser rabin-4 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+python run-qcomp.py dtcontrol-parser resource-gathering-5 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
+python run-qcomp.py dtcontrol-parser wlan-1-2 --output_dir experiments/dtcontrol-qcomp-discounted/ --input-path environments/qcomp-discounted/
