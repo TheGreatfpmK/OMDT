1b80be2abcb05086b0413a0a464d586ec91dd012
Filip Macak <filip.macak1999@gmail.com>
2025-04-14 16:22:10 +0200

    fixing path

--- experiments-dts-cav-omdt.py
+++ experiments-dts-cav-omdt.py
@@ -56,7 +56,7 @@ def main(omdt_dir, models_dir, workers, timeout, maxmem, output, experiment_name
     for model in models:
         model_tasks = []
         for d in range(depth_min,depth_max+1):
-            task = (f"python3 run-experiment.py omdt {model} --seed 0 --gamma {0.99 if model not in list(different_gamma.keys()) else different_gamma[model]} --max_depth {d} --time_limit {timeout} --output_dir /opt/cav25-experiments/results/logs/{experiment_group_name}/ --verbose 1 --model-file-name {"model-random-enabled.drn" if model in qcomp_models else "model-random.drn"}", f"/opt/cav25-experiments/results/logs/{experiment_group_name}/{model}/log-depth-{d}.log", f"model {model_count}/{len(models)} depth {d}/{depth_max} - {model} -")
+            task = (f"python3 run-experiment.py omdt {model} --input-path {models_dir} --seed 0 --gamma {0.99 if model not in list(different_gamma.keys()) else different_gamma[model]} --max_depth {d} --time_limit {timeout} --output_dir /opt/cav25-experiments/results/logs/{experiment_group_name}/ --verbose 1 --model-file-name {"model-random-enabled.drn" if model in qcomp_models else "model-random.drn"}", f"/opt/cav25-experiments/results/logs/{experiment_group_name}/{model}/log-depth-{d}.log", f"model {model_count}/{len(models)} depth {d}/{depth_max} - {model} -")
             model_tasks.append(task)
             all_log_paths.append(f"/opt/cav25-experiments/results/logs/{experiment_group_name}/{model}/log-depth-{d}.log")
 
